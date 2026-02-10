bfb6d436a257dbc7407bb97308029338e3839d13
DESKTOP-2HN8GNS\it <tkarjs0303@gmail.com>
2026-02-10 10:25:48 +0900

    관리자 페이지 사이드바 액티브 표시 다시 추가.

--- src/main/java/bitc/next502/team_prj/controller/MngController.java
+++ src/main/java/bitc/next502/team_prj/controller/MngController.java
@@ -36,6 +36,7 @@ public class MngController {
 
         String role = (String) session.getAttribute("role");
         Object userBoxing = session.getAttribute("loginUser");
+        model.addAttribute("menuId", "menu");
 
         if (role == null || userBoxing == null) {
             return "redirect:/login";
@@ -87,6 +88,7 @@ public class MngController {
         // 세션 검증
         String role = (String) session.getAttribute("role");
         Object userBoxing = session.getAttribute("loginUser");
+        model.addAttribute("menuId", "store");
 
         if (role == null || userBoxing == null || !"BUSINESS".equals(role)) {
             return "redirect:/login";
@@ -116,7 +118,8 @@ public class MngController {
     }
 
     @GetMapping("/mngreview")
-    public String mngreview() {
+    public String mngreview(Model model) {
+        model.addAttribute("menuId", "review");
         return "mng/mngreview";
     }
 
