615ab7cc3ab3b76fa7aa70d9a73c3c83389140a3
DESKTOP-PVRSAS2\it <wlsrud2308@gmail.com>
2026-02-11 09:11:32 +0900

    관리자 페이지 수정

--- src/main/java/bitc/next502/team_prj/controller/ReviewController.java
+++ src/main/java/bitc/next502/team_prj/controller/ReviewController.java
@@ -1,11 +1,10 @@
 package bitc.next502.team_prj.controller;
 
-import bitc.next502.team_prj.dto.MyInfoDTO;
 import bitc.next502.team_prj.dto.ReviewDTO;
-import bitc.next502.team_prj.service.MyPageServiceImpl;
 import bitc.next502.team_prj.service.ReviewService;
 import com.github.pagehelper.PageInfo;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -14,105 +13,86 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
+
 @Controller
 public class ReviewController {
 
     @Autowired
     private ReviewService reviewService;
 
-    @Autowired
-    private MyPageServiceImpl myPageService;
-
-    @Autowired
-    private bitc.next502.team_prj.service.BookmarkService bookmarkService;
-
-    // 1. 식당 리뷰 리스트
+    //레스토랑에 관련된 댓글리스트 @RequestParam("id") String id, Model model
     @GetMapping("/review/reviewList")
     public ModelAndView reviewList(@RequestParam("restaurantId") String restaurantId) throws Exception{
+
         if(restaurantId.equals("") || restaurantId == null){
-            restaurantId = "Buk7096";
+            restaurantId = "Buk7096";  //gang3669   gang8428
         }
+
         List<ReviewDTO> reviewList = reviewService.selectReviewsList(restaurantId);
         ModelAndView mv = new ModelAndView("review/reviewList");
         mv.addObject("reviewList", reviewList);
+
         return mv;
     }
 
-    // 2. 리뷰 상세
+
+    //레스토랑에 관련된 댓글 상세보기
     @GetMapping("/review/reviewDetail/{reviewIdx}")
     public ModelAndView reviewDetail(@PathVariable("reviewIdx") int reviewId) throws Exception{
-        ReviewDTO review = reviewService.selectReviewsDetail(reviewId);
+
+        ReviewDTO review=reviewService.selectReviewsDetail(reviewId);
         ModelAndView mv = new ModelAndView("review/reviewDetail");
         mv.addObject("review", review);
         return mv;
+
     }
-
-
-    // 3. 내가 쓴 리뷰 (유저 정보 추가)
-    @RequestMapping("/review/myreviewList")
-    public ModelAndView myReviewList(@RequestParam("userId") String userId) throws Exception{
-
     //Mypage에서 조회하는 내가 쓴 댓글들 페이지처리
     @RequestMapping("/review/myreviewList")
     public ModelAndView myReviewList(@RequestParam(required = false,defaultValue="1",value="pageNum") int pageNum,
                                      @RequestParam("userId") String userId) throws Exception{
         int navigatePages = 5; //화면에 보여지는 페이지버튼수
         PageInfo<ReviewDTO> reviewList = new PageInfo<>( reviewService.selectMyReviewsList(pageNum,userId),navigatePages);
-
         ModelAndView mv = new ModelAndView("review/myreviewList");
-
-        // (1) 리뷰 목록 가져오기
-        List<ReviewDTO> reviewList = reviewService.selectMyReviewsList(userId);
         mv.addObject("reviewList", reviewList);
-
-        // (2) 로그인한 유저 정보 가져오기
-        MyInfoDTO userInfo = myPageService.getMyInfo(userId);
-
-        // 안전장치
-        if (userInfo == null) {
-            userInfo = new MyInfoDTO();
-            userInfo.setName("회원");
-            userInfo.setGrade("일반");
-        }
-
-        // (3) 화면으로 정보 보내기
-        List<bitc.next502.team_prj.dto.BookmarkDTO> bookmarkList = bookmarkService.getBookmarkList(userId);
-        mv.addObject("bookmarkList", bookmarkList);
-        mv.addObject("userInfo", userInfo);
-
         return mv;
+
     }
 
+    //댓글 등록 view
     @GetMapping("/review/reviewWrite")
     public String writeReview() {
         return "review/reviewWrite";
     }
 
+    //댓글 등록처리
     @PostMapping("/review/reviewWrite")
     public String writeReview(ReviewDTO review) throws Exception{
         reviewService.insertReview(review);
         return "redirect:/mypage/main";
     }
-
+    //댓글 등록처리 파일 포함
     @PostMapping("/review/reviewWriteFile")
-    public String writeReviewFile(ReviewDTO review, MultipartHttpServletRequest multipart, HttpServletRequest req) throws Exception{
-        reviewService.insertReviewFile(review, multipart);
+    public String writeReviewFile(ReviewDTO review,
+                                  MultipartHttpServletRequest multipart,
+                                  HttpServletRequest req) throws Exception{
+
+        reviewService.insertReviewFile(review,multipart);
+
         return "redirect:/mypage/main";
     }
-
+    //예약 방운후 댓글 등록처리 파일 포함
     @PostMapping("/review/reviewWriteReservation")
-    public String reviewWriteReservation(ReviewDTO review, MultipartHttpServletRequest multipart, HttpServletRequest req) throws Exception{
-        reviewService.insertReviewFile(review, multipart);
+    public String reviewWriteReservation(ReviewDTO review,
+                                         MultipartHttpServletRequest multipart,
+                                         HttpServletRequest req) throws Exception{
+
+        reviewService.insertReviewFile(review,multipart);
+
         return "redirect:/mypage/main";
     }
 
+    //댓글 수정
     @PostMapping("/review/reviewUpdate")
-
-    public String reviewUpdate(ReviewDTO review, MultipartHttpServletRequest multipart, HttpServletRequest req) throws Exception{
-        reviewService.updateReview(review, multipart);
-        return "redirect:/mypage/main";
-    }
-
     public String reviewUpdate(ReviewDTO review,
                                MultipartHttpServletRequest multipart,
                                HttpServletRequest req) throws Exception{
@@ -125,7 +105,13 @@ public class ReviewController {
     //댓글 삭제
     @RequestMapping("/review/delReview")
     public String delReview(@RequestParam("reviewIdx") int reviewIdx) throws Exception{
+
         reviewService.deleteReview(reviewIdx);
         return "redirect:/mypage/main";
+
     }
+
+
+
+
 }
